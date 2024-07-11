0738db091806c599826112dbbb5e128102d896db
Ashutosh <wwwashutosh225@gmail.com>
2024-07-11 08:38:53 +0530

    some ui fixes

--- src/routes/Passwords.jsx
+++ src/routes/Passwords.jsx
@@ -68,16 +68,16 @@ function Passwords() {
 
     return (
         <div className="min-h-screen flex flex-col items-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 px-2 sm:px-4 lg:px-6">
-            <h1 className="text-2xl sm:text-3xl font-bold text-center mb-4 sm:mb-8">Your Passwords</h1>
+            <h1 className="text-2xl sm:text-3xl font-bold text-center my-4">Your Passwords</h1>
             {passArray.length === 0 ? (
                 <h2 className="text-center text-base sm:text-lg">No Passwords to show.</h2>
             ) : (
-                <div className="overflow-x-auto w-full lg:w-3/4 xl:w-2/3">
+                <div className="overflow-x-auto w-full lg:w-3/4 xl:w-2/3 rounded-md">
                     <table className="min-w-full bg-white dark:bg-gray-800 shadow-md rounded-lg mx-auto">
                         <thead className="bg-gradient-to-r from-blue-500 to-teal-500 text-white">
                             <tr>
-                                <th className="p-2 sm:p-4 text-left text-xs sm:text-sm">#</th>
-                                <th className="p-2 sm:p-4 text-left text-xs sm:text-sm">Actions</th>
+                                <th className="p-2 sm:p-4 text-left text-xs sm:text-sm w-6">#</th>
+                                <th className="p-2 sm:p-4 text-left text-xs sm:text-sm w-12 md:w-24">Actions</th>
                                 <th className="p-2 sm:p-4 text-left text-xs sm:text-sm">Details</th>
                             </tr>
                         </thead>
@@ -85,17 +85,17 @@ function Passwords() {
                             {passArray.map((password, index) => (
                                 <tr key={index} className="border-b border-gray-200 dark:border-gray-700">
                                     <td className="p-2 sm:p-4 text-xs sm:text-sm">{index + 1}</td>
-                                    <td className="p-2 sm:p-4 text-xs sm:text-sm flex flex-col items-center space-y-2 w-12">
+                                    <td className="p-2 sm:p-4 text-xs sm:text-sm flex flex-col items-center space-y-2 w-full ">
                                         <button
                                             onClick={() => handleDelete(password.uniqueId)}
-                                            className="bg-red-500 text-white rounded-full px-1 sm:px-2 py-1 hover:bg-red-700 transition duration-300"
+                                            className="bg-red-500 text-white rounded-lg px-1 sm:px-2 py-1 hover:bg-red-700 transition duration-300"
                                         >
                                             Delete
                                         </button>
                                         <Link to={'/'}>
                                             <button
                                                 onClick={() => dispatch(setUpdateFormState(password))}
-                                                className="bg-blue-500 text-white rounded-full px-1 sm:px-2 py-1 hover:bg-blue-700 transition duration-300"
+                                                className="bg-blue-500 text-white rounded-lg px-1 sm:px-2 py-1 hover:bg-blue-700 transition duration-300"
                                             >
                                                 Update
                                             </button>
